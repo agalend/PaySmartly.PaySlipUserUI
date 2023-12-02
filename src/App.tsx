b698d246241bab9697ecee7aa3101ab71c7edb13
stefan <stefan.bozov@outlook.com>
2023-12-02 17:12:21 +0200

    add create pay slip example

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,23 @@ import './App.css';
 import restApiClient from './api/client';
 
 function App() {
+  // restApiClient.create({
+  //   employee: { firstName: "Dragan", lastName: "Draganov" },
+  //   annualSalary: 60050,
+  //   superRate: 9,
+  //   payPeriodFrom: new Date(),
+  //   payPeriodTo: new Date(),
+  //   roundTo: 2,
+  //   months: 12,
+  //   requester: { firstName: "Unknown", lastName: "Unknown" },
+  // })
+  //   .then((result) => {
+  //     const r = result;
+  //   })
+  //   .catch(error => {
+  //     const e = error;
+  //   });
+
   return (
     <div className="App">
       <header className="App-header">
