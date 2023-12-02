934a26ddbd22c387c566f280bb4b4a5b3888914e
stefan <stefan.bozov@outlook.com>
2023-12-02 17:06:14 +0200

    implement rest client api

--- src/api/client.ts
+++ src/api/client.ts
@@ -5,7 +5,7 @@ import axios, { AxiosInstance } from 'axios';
 export interface ApiClient {
     create(request: Request): Promise<Response>;
     get(id: string): Promise<Response>;
-    delete(id: string): Promise<boolean>;
+    delete(pattern: string): Promise<boolean>;
 
     getAllForEmployee(firstName: string, lastName: string, limit: number, offset: number): Promise<Array<Response>>;
     getAllForAnnualSalary(from: number, to: number, limit: number, offset: number): Promise<Array<Response>>;
@@ -23,41 +23,41 @@ export class RestApiClient implements ApiClient {
     }
 
     public async create(request: Request): Promise<Response> {
-        const pattern = "calculations/pay-slips";
-        const response = await this.axiosInstance.post<Response>(pattern, request);
+        const url = "calculations/pay-slips";
+        const response = await this.axiosInstance.post<Response>(url, request);
         return response.data;
     }
 
     public async get(id: string): Promise<Response> {
-        const pattern = `archive/pay-slips/${id}`;
-        const response = await this.axiosInstance.get<Response>(pattern);
+        const url = `archive/pay-slips/${id}`;
+        const response = await this.axiosInstance.get<Response>(url);
         return response.data;
     }
-    public async delete(id: string): Promise<boolean> {
-        const pattern = `archive/pay-slips/${id}`;
-        const response = await this.axiosInstance.delete<void>(pattern);
+
+    public async delete(url: string): Promise<boolean> {
+        const response = await this.axiosInstance.delete<void>(url);
         return response.status === 204;
     }
 
     public async getAllForEmployee(firstName: string, lastName: string, limit: number, offset: number): Promise<Array<Response>> {
-        const pattern = `archive/pay-slips/employee`;
-        const response = await this.axiosInstance.get<Array<Response>>(pattern, {
+        const url = `archive/pay-slips/employee`;
+        const response = await this.axiosInstance.get<Array<Response>>(url, {
             params: { firstName, lastName, limit, offset }
         });
         return response.data;
     }
 
     public async getAllForAnnualSalary(from: number, to: number, limit: number, offset: number): Promise<Array<Response>> {
-        const pattern = `archive/pay-slips/annual-salary`;
-        const response = await this.axiosInstance.get<Array<Response>>(pattern, {
+        const url = `archive/pay-slips/annual-salary`;
+        const response = await this.axiosInstance.get<Array<Response>>(url, {
             params: { from, to, limit, offset }
         });
         return response.data;
     }
 
     public async getAllForSuperRate(from: number, to: number, limit: number, offset: number): Promise<Array<Response>> {
-        const pattern = `archive/pay-slips/super-rate`;
-        const response = await this.axiosInstance.get<Array<Response>>(pattern, {
+        const url = `archive/pay-slips/super-rate`;
+        const response = await this.axiosInstance.get<Array<Response>>(url, {
             params: { from, to, limit, offset }
         });
         return response.data;
