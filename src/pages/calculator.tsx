9d23d5e666ce65acc5a49e94888525ccd629cdc5
stefan <stefan.bozov@outlook.com>
2023-12-02 20:01:29 +0200

    create calculator form

--- src/pages/calculator.tsx
+++ src/pages/calculator.tsx
@@ -1,22 +1,37 @@
-import React from "react";
+import Stack from 'react-bootstrap/Stack';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const Calculator = () => {
     return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "centre",
-                alignItems: "centre",
-                height: "100vh",
-            }}
-        >
+        <Stack gap={10}>
             <div>
-                div 11
+                <Form.Control type="text" placeholder="First Name" style={{ width: "30%" }} />
+                <br />
+                <Form.Control type="text" placeholder="Second Name" style={{ width: "30%" }} />
+                <br />
+                <Form.Control type="number" placeholder="Annual Salary" style={{ width: "30%" }} />
+                <br />
+                <Form.Control type="number" placeholder="Super Rate" style={{ width: "30%" }} />
+                <br />
+                <Button variant="outline-secondary">Calculate</Button>{' '}
             </div>
+            <br />
+            <br />
+            <br />
+            <br />
             <div>
-                div 2
+            <Form.Control type="text" placeholder="Name" style={{ width: "30%" }} readOnly/>
+                <br />
+                <Form.Control type="number" placeholder="Gross Income" style={{ width: "30%" }} readOnly/>
+                <br />
+                <Form.Control type="number" placeholder="Income Tax" style={{ width: "30%" }} readOnly/>
+                <br />
+                <Form.Control type="number" placeholder="Net Income" style={{ width: "30%" }} readOnly/>
+                <br />
+                <Form.Control type="number" placeholder="Super" style={{ width: "30%" }} readOnly/>
             </div>
-        </div>
+        </Stack>
     );
 };
 
